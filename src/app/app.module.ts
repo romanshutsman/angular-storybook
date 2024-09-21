61a2d8bfc22f7076c994da1d663ef20a70df05e9
shutsmanroman <roman13275@gmail.com>
2024-09-21 12:56:55 +0300

    implement task list interactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { TaskModule } from './components/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
@@ -10,18 +9,17 @@ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
+import InboxScreenComponent from './components/inbox-screen/inbox-screen.component';
+import PureInboxScreenComponent from './components/pure-inbox-screen/pure-inbox-screen.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InboxScreenComponent, PureInboxScreenComponent],
   imports: [
     BrowserModule,
     TaskModule,
-    NgxsModule.forRoot([], {
-      developmentMode: !environment.production,
-    }),
+    NgxsModule.forRoot([], { developmentMode: !environment.production, }),
     NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot({
-      disabled: environment.production,
-    }),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production, }),
   ],
   providers: [],
   bootstrap: [AppComponent],
